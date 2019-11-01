11f5b2b1dc06bc8c63ce263d956ff59eb87b0eb3
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-01 11:58:59 +0100

    created endpoints

--- actionModelRouter.js
+++ actionModelRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const actions = require('./data/helpers/actionModel');
 const router = express.Router();
+const projects = require('./data/helpers/projectModel');
 
 router.get('/', (req, res) => {
     actions.get()
@@ -12,6 +13,8 @@ router.get('/', (req, res) => {
         })
 
 })
+
+// router.get('/:id/actions', (req, res) )
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params
@@ -29,44 +32,77 @@ router.delete('/:id', (req, res) => {
         })
 })
 
-router.post('/:id', (req, res) => {
-    const { id } = req.params
-    const action = { ...req.body, project_id: id }
-
-    if (action.description && action.notes && action.project_id) {
-        actions.insert(action)
-            .then(action => {
-                res.status(201).json({ action });
-            })
-            .catch(error => {
-                res.status(500).json({
-                    error
-                });
-            });
-    } else {
-        res.status(400).json({ error: "Please provide project_id, description and notes for the action." });
-    }
-
+router.post('/:id', validateProjectId, validateAction, (req, res) => {
+    const { description, notes, completed } = req.body
+    actions.insert({
+        project_id: req.project.id, description, notes, completed
+    })
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'internal server error' + error })
+        })
 })
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params
-    const action = { ...req.body, project_id: id }
-
-    if (action.description && action.notes && action.project_id) {
-        actions.update(action)
-            .then(action => {
-                res.status(201).json({ action });
-            })
-            .catch(error => {
-                res.status(500).json({
-                    error
-                });
-            });
-    } else {
-        res.status(400).json({ error: "Please provide project_id, description and notes for the action." });
-    }
-
+router.put('/:id', validateActionId, validateAction, (req, res) => {
+  actions.update(req.action.id, req.body)
+  .then(() => {
+      res.status(200).json({...req.action, ...req.body})
+  })
+  .catch(() => {
+      res.status(500).json({ errorMessage: 'internal server error'})
+  })
 })
+
+function validateAction(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if (req.body.notes && req.body.description) {
+            next();
+
+        }
+        else {
+            res.status(400).json({ message: "all fields are required!" });
+        }
+
+    }
+    else {
+        res.status(400).json({ message: "missing action data" });
+    }
+}
+
+function validateProjectId(req, res, next) {
+    const id = req.params.id;
+    projects.get(id)
+        .then(project => {
+            if (project) {
+
+                req.project = project;
+                next()
+            } else {
+                res.status(404).json({ message: 'Project id does not correspond with an actual Project' });
+            }
+        })
+        .catch(error => {
+            res.status(404).json({ message: "invalid project id: " + error.message })
+        })
+
+}
+
+function validateActionId(req, res, next) {
+    actions.get(req.params.id)
+        .then(action => {
+            if (action) {
+                req.action = action;
+                next()
+            } else {
+                res.status(404).json({ message: 'Action id does not correspond with an actual Action' });
+            }
+        })
+        .catch(error => {
+            res.status(404).json({ message: "invalid Action id: " + error.message })
+        })
+
+}
 
 module.exports = router
--- projectModelRouter.js
+++ projectModelRouter.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     projects.get()
-    .then(project => {
-        res.status(200).json(project)
-    })
-    .catch(() => {
-        res.status(500).json({ errorMessage: 'internal server error' })
-    })
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(() => {
+            res.status(500).json({ errorMessage: 'internal server error' })
+        })
 })
 
 router.get('/:id/actions', (req, res) => {
@@ -38,7 +38,7 @@ router.post('/', (req, res) => {
                 res.status(201).json(project)
             })
             .catch(error => {
-                res.status(500).json({errorMessage: 'internal server error' + error})
+                res.status(500).json({ errorMessage: 'internal server error' + error })
             })
     }
 })
@@ -46,34 +46,54 @@ router.post('/', (req, res) => {
 router.delete('/:id', (req, res) => {
     const { id } = req.params
     projects.remove(id)
-    .then(project => {
-        if(project && project > 0) {
-            res.status(200).json({ success: `post ${id} was successfully deleted`})
-        }
-        else{
-            res.status(404).json({ message: 'the project with that id does not exist'})
-        }
-    })
-    .catch(error => {
-        res.status(500).json({errorMessage: 'internal server error' + error })
-    })
+        .then(project => {
+            if (project && project > 0) {
+                res.status(200).json({ success: `post ${id} was successfully deleted` })
+            }
+            else {
+                res.status(404).json({ message: 'the project with that id does not exist' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: 'internal server error' + error })
+        })
 })
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const body = req.body;
     projects.update(id, body)
-    .then(project => {
-        if(project) {
-            res.status(200).json(project)
-        }
-        else{
-            res.status(400).json({ error: `the id ${id} is not valid` })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({errorMessage: 'internal server error' + error })
-    })
+        .then(project => {
+            if (project) {
+                res.status(200).json(project)
+            }
+            else {
+                res.status(400).json({ error: `the id ${id} is not valid` })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: 'internal server error' + error })
+        })
 })
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.project)
+})
+
+function validateProjectId(req, res, next) {
+    projects.get(req.params.id)
+        .then(project => {
+            if (project) {
+                req.project = project;
+                next()
+            } else {
+                res.status(404).json({ message: 'Project id does not correspond with an actual Project' });
+            }
+        })
+        .catch(error => {
+            res.status(404).json({ message: "invalid project id: " + error.message })
+        })
+
+}
 
 module.exports = router
